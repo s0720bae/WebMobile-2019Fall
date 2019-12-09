efd0cfeae8364f794d838a31fc999cfaca9f30bd
Daniel Bae <31868920+s0720bae@users.noreply.github.com>
2019-12-09 15:39:24 -0600

    last version of lab

--- 2019Fall-Web/Lab2/Source/MobileLab/app/src/main/java/com/example/mobilelab/MainActivity.java
+++ 2019Fall-Web/Lab2/Source/MobileLab/app/src/main/java/com/example/mobilelab/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
